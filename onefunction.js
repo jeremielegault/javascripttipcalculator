f4671fefc2a2fddee95f57bb1fb7577802dde026
“Jeremie <jeremie_legault@hotmail.com>
2021-11-30 10:34:21 -0500

    javascript added

---
+++ onefunction.js
@@ -0,0 +1,6 @@
+// Hide the tip amount on load
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
+
+// click to call function
+document.getElementById("calculate").onclick = calculateTip();
--- tipcalculator.js
+++ tipcalculator.js
@@ -1,15 +1,15 @@
-//Calculate Tip
-function calculateTip() {
-  var billAmt = document.getElementById("billamt").value;
-  var serviceQual = document.getElementById("serviceQual").value;
-  var numOfPeople = document.getElementById("peopleamt").value;
+// Calculate the tip
+const calculateTip = () => {
+  let billAmt = document.getElementById("billamt").value;
+  let serviceQual = document.getElementById("serviceQual").value;
+  let numOfPeople = document.getElementById("peopleamt").value;
 
-  //validate input
+  //   validate input
   if (billAmt === "" || serviceQual == 0) {
     alert("Please enter values");
     return;
   }
-  //Check to see if this input is empty or less than or equal to 1
+  // Check to see if this input is empty or less than or equal to 1
   if (numOfPeople === "" || numOfPeople <= 1) {
     numOfPeople = 1;
     document.getElementById("each").style.display = "none";
@@ -17,22 +17,22 @@ function calculateTip() {
     document.getElementById("each").style.display = "block";
   }
 
-  //Calculate tip
-  var total = (billAmt * serviceQual) / numOfPeople;
-  //round to two decimal places
+  // Calculate tip
+  let total = (billAmt * serviceQual) / numOfPeople;
+  // round to two decimal places
   total = Math.round(total * 100) / 100;
-  //next line allows us to always have two digits after decimal point
+  // Next line allows us to have two digits after the decimal point
   total = total.toFixed(2);
-  //Display the tip
+  // Display the tip
   document.getElementById("totalTip").style.display = "block";
   document.getElementById("tip").innerHTML = total;
-}
+};
 
-//Hide the tip amount on load
+// Hide the tip amount on load
 document.getElementById("totalTip").style.display = "none";
 document.getElementById("each").style.display = "none";
 
-//click to call function
+// click to call function
 document.getElementById("calculate").onclick = function () {
   calculateTip();
 };
