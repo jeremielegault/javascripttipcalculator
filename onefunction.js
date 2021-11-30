4a5153d8238f66bc07b7467c7dd01bed6dbd4ab0
“Jeremie <jeremie_legault@hotmail.com>
2021-11-30 12:34:06 -0500

    last commit

--- onefunction.js
+++
@@ -1,6 +0,0 @@
-// Hide the tip amount on load
-document.getElementById("totalTip").style.display = "none";
-document.getElementById("each").style.display = "none";
-
-// click to call function
-document.getElementById("calculate").onclick = calculateTip();
